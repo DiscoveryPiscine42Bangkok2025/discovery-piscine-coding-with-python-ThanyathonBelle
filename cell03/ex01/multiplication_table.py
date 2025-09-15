473cf0bb396253464023b20eaa4d087c759029b5
Thanyathon <109953990+ThanyathonBelle@users.noreply.github.com>
2025-09-15 22:24:03 +0700

    Add files via upload

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,3 @@
+num = int(input("Enter a number "))
+for i in range(0, 10):
+    print(num, "x", i, "=", num * i)
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+while True:
+    user = input("What you gotta say? : ")
+    if user == "STOP":
+        break
+    else:
+        print("I got that! Anything else? :")
